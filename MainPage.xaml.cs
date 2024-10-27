60ee1cfc9e98f64e30e3b04b59c138bc269f4c18
nafi.dhe21 <nafi.dhe21@gmail.com>
2024-10-27 23:55:24 +0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChildGrowthStandardApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChildGrowthStandardApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
